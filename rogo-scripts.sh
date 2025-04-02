ca16b41927d42a42c504133566b830d012a39a48
Sergei Solagaian <ssolagaian@gmail.com>
2025-04-02 21:05:42 +0300

    add rogo-scripts

---
+++ rogo-scripts.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function sshk() {
+  #export DISABLE_AUTO_TITLE="true"
+  #echo -n -e "\033]0;$1\007"
+  sshpass -p 'RogoNuvo' ssh -o "StrictHostKeyChecking no" "rogocompute@$1"
+}
+
+function sftpk() {
+  sshpass -p 'RogoNuvo' sftp -o "StrictHostKeyChecking no" "rogocompute@$1"
+}
+
+qrcopy() {
+  qrencode -t ansiutf8 $1
+}
+
+boxqr() {
+  qrcopy "https://lab.rogoag.com/$1"
+}
+
+randchars() {
+  tr </dev/urandom -dc $1 | head -c$2
+  echo
+}
+
+randbox() {
+  boxqr "$(randchars a-z0-9 10)"
+}
+
+randnum() {
+  randchars 0-9 $1
+}
+
+randrogo() {
+  echo "RG$(randnum 12)"
+}
+
+randbag() {
+  qrcopy "$(randrogo)"
+}
